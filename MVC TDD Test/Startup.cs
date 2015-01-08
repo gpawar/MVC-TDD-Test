514b05b32514472d7d390dd66f9e035a218375b6
thatcoderguy <davie@recallhosting.co.uk>
2015-01-08 07:36:33 +0000

    Initial

---
+++ MVC TDD Test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_TDD_Test.Startup))]
+namespace MVC_TDD_Test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
